f375d8b743c2ebf0b4b0ba069cbf83c792d1209e
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-28 11:02:54 -0300

    Verifica token

--- src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
+++ src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
@@ -1,4 +1,35 @@
 package br.com.forum_hub.infra.security;
 
-public class FiltroTokenAcesso {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class FiltroTokenAcesso extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        //recuperar token da requsição
+        String token = recuperarTokenRequisicao(request);
+
+        if (token != null) {
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarTokenRequisicao(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
 }
