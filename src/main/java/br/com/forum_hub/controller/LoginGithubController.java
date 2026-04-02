6a158ae3fb0cfe91fe5e6885ba59000367d140b8
MatheusDoRosario <matheusxdargws@gmail.com>
2026-04-02 11:18:30 -0300

    Authentica o usuario

--- src/main/java/br/com/forum_hub/controller/LoginGithubController.java
+++ src/main/java/br/com/forum_hub/controller/LoginGithubController.java
@@ -1,10 +1,17 @@
 package br.com.forum_hub.controller;
 
+import br.com.forum_hub.domain.autenticacao.DadosToken;
+import br.com.forum_hub.domain.autenticacao.TokenService;
 import br.com.forum_hub.domain.autenticacao.github.LoginGithubService;
+import br.com.forum_hub.domain.usuario.Usuario;
+import br.com.forum_hub.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +26,12 @@ public class LoginGithubController {
     @Autowired
     private LoginGithubService loginGithubService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
     @GetMapping
     public ResponseEntity<Void> redirecionarGithub() {
         var url = loginGithubService.gerarUrl();
@@ -29,8 +42,16 @@ public class LoginGithubController {
     }
 
     @GetMapping("/autorizado")
-    public ResponseEntity<String> obterToken(@RequestParam String code) {
-        var token = loginGithubService.obterEmail(code);
-        return ResponseEntity.ok(token);
+    public ResponseEntity<DadosToken> autenticarUsuarioOAuth(@RequestParam String code) {
+        var email = loginGithubService.obterEmail(code);
+
+        var usuario = usuarioRepository.findByEmailIgnoreCaseAndVerificadoTrue(email).orElseThrow();
+        var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String tokenAcesso = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        String refreshToken = tokenService.gerarRefreshToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken));
     }
 }
